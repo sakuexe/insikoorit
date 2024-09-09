ce7db22d007905ba450748ede59f2a152ea40059
sakuexe <saku.karttunen@gmail.com>
2024-09-09 14:09:06 +0300

    reorganizing the repo

--- assignments/2048-python/AI_heuristics.py
+++ assignments/2048-python/AI_heuristics.py
@@ -1,7 +1,8 @@
 import constants as c
 import random
 
+
 def AI_play():
-    tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT] 
-    key=tmp[random.randint(0,3)]
+    tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT]
+    key = tmp[random.randint(0, 3)]
     return key
