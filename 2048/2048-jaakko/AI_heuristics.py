e6ee9300516b11bb46ba379e17fbdac07c77a043
Jaakko Saarikko <jaakko.saarikko@protonmail.com>
2024-09-10 12:05:25 +0300

    isoin alas oikealle heuristiikka

--- 2048/2048-jaakko/AI_heuristics.py
+++ 2048/2048-jaakko/AI_heuristics.py
@@ -2,45 +2,70 @@ import constants as c
 import random
 import logic
 
-commands = {c.KEY_UP: logic.up,
-            c.KEY_DOWN: logic.down,
-            c.KEY_LEFT: logic.left,
-            c.KEY_RIGHT: logic.right}
+commands = {
+    c.KEY_UP: logic.up,
+    c.KEY_DOWN: logic.down,
+    c.KEY_LEFT: logic.left,
+    c.KEY_RIGHT: logic.right
+}
 
 def AI_play(matrix):
-   
-    #key = heuristic_random()
-    key = heuristic_empty_tile(matrix)
+    scores = {
+        c.KEY_UP: 0,
+        c.KEY_DOWN: 0,
+        c.KEY_LEFT: 0,
+        c.KEY_RIGHT: 0
+    }
 
+    for direction, command in commands.items():
+        game, done, points = command(matrix)
+        if game == matrix:
+            scores[direction] = 0
+            continue
+        
+        #tempScore = 
+        tempScore = 3 * heuristic_empty_tiles(game)
+        tempScore += heuristic_biggest_tile_down_right(game)  # Suurimman palikan siirtäminen oikealle alas
+
+        scores[direction] = tempScore
+    
+    key = max(scores, key=scores.get)  # Valitaan paras suunta
     return key
 
-def heuristic_random():
-    tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT] 
-    key=tmp[random.randint(0,3)]
-    return key
+def heuristic_biggest_tile_down_right(matrix):
+    biggest_tile = 0
+    biggest_tile_position = (0,0)
 
-def heuristic_empty_tile(matrix):
-    best_score = -1
-    return_key = None
-    for key in commands.keys():
-        game, done, points = commands[key](matrix)  
+    for i in range(c.GRID_LEN):
+        for j in range(c.GRID_LEN):
+            if matrix[i][j] > biggest_tile:
+                biggest_tile = matrix[i][j]
+                biggest_tile_position = (i,j)
 
-        if not done:
-           pass
+    row, col = biggest_tile_position
+    score = 0
 
-        if done:
-            n_empty=0
-            for i in range(c.GRID_LEN):
-                for j in range(c.GRID_LEN):
-                    if game[i][j]==0:
-                        n_empty+=1
-            if n_empty > best_score:
-                best_score = n_empty
-                return_key = key
-           
+    # Palkitaan, jos suurin palikka on oikealla alhaalla
+    if row == c.GRID_LEN - 1 and col == c.GRID_LEN - 1:
+        score += 1000  # Palkitaan voimakkaasti, jos palikka on oikeassa alakulmassa
+    
+    # Palkitaan, jos suurin palikka on oikealla
+    if col == c.GRID_LEN - 1:
+        score += 500
+    
+    # Palkitaan, jos suurin palikka on alhaalla
+    if row == c.GRID_LEN - 1:
+        score += 500
 
-    print("Best move seems to be: ")
-    print(return_key)    
+    # Rankaiseminen, jos suurin palikka on kaukana oikeasta alakulmasta
+    distance_from_bottom_right = abs(row - (c.GRID_LEN - 1)) + abs(col - (c.GRID_LEN - 1))
+    score -= distance_from_bottom_right * 100  # Rankaise etäisyyden mukaan
 
-    return return_key
+    return score
 
+def heuristic_empty_tiles(matrix):
+    empty_tile = 0
+    for row in matrix:
+        empty_tile += row.count(0)
+    
+    return empty_tile 
