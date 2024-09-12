c9404182980037f206360851f643e31c5e0f1a93
Samppa72 <samppa.sirno@student.hamk.fi>
2024-09-12 20:19:54 +0300

    UUSI versio
    Muutokset niin että pelin vaatimusten mukainen heurestiikka ja Puzzle.pyhyn (että näyttää tuloksen)

--- 2048/2048-python_Samppa/AI_heuristics.py
+++ 2048/2048-python_Samppa/AI_heuristics.py
@@ -1,8 +1,14 @@
 import constants as c
 import random
+import logic
 
+commands = {c.KEY_UP: logic.up,
+            c.KEY_DOWN: logic.down,
+            c.KEY_LEFT: logic.left,
+            c.KEY_RIGHT: logic.right}
 
-def AI_play(puz, matrix):
+def AI_play(matrix):
+    #Samppa
     tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT]
     #Samppa         
     key = c.KEY_LEFT
@@ -12,68 +18,44 @@ def AI_play(puz, matrix):
     #N = max number of tries Samppa
     
     for k in range(4):
-        N = 0
-        tried = tmp
-        GO = False
-        while tried != [0,0,0,0] and N <8600:
-            N = N + 1
-            #print(N)
-            #ADDED matrix3, self.matrix (matrix3 result of a tryed key Samppa
-            matrix3, done, points = puz.commands[key](matrix) 
-            if tried[k]!=0:           
-                po = AI_play2(matrix, matrix3, k, key)
-                if po > maxp:
-                    key = tmp[k]
-                    maxp = po
-                    #tried[k] = 0
-            else:
-                for i in range (4):
-                    if tried[i] != 0:
-                        key = tried[i]
-            """debug
-            print (tried)
-            print(key, maxp)
-            """
-            #print("!", self.matrix, matrix3)
-            if matrix == matrix3:
-                tried[k] = 0
-        if matrix == matrix3:
-            GO = True
-            for a in range(2):
-                """
-                k=k+1
-                if k > 3:
-                    k = 1
-                """
-                ke = tmp[a+1]
-                if ke == 0:
-                    ke = 'Down'
-                print(a, ke)
-                matrix2, done, points = puz.commands[ke](matrix) 
-                if matrix2 != matrix:
-                    GO = False
-                key = ke
-    return key, GO
-    """        
-            #tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT] 
-            #key=tmp[random.randint(0,3)]
-            #commented Samppa
-            #self.commands[key](self.matrix)
-            #Samppa
-            matrix2=self.matrix
-            # ORIGINAL
-            self.matrix, done, points = self.commands[key](self.matrix)
-            #ORIGINAL
-            self.points += points
-            #Samppa
-            print("#", done)
-            GS = 'Not'
-            if self.matrix == matrix2: 
-                self.game_over = True
-                done = True
-                GS = 'lose'
-                print(GS)
-    """        
+            #N = 0
+            #tried = tmp
+            
+            #while tried != [0,0,0,0] and N <8600:
+            #    N = N + 1
+                #print(N)
+                #ADDED matrix3, self.matrix (matrix3 result of a tryed key Samppa
+        matrix3, done, points = commands[key](matrix) 
+        #if tried[k]!=0:           
+        po = AI_play2(matrix, matrix3, k, key)
+        if po > maxp:
+            key = tmp[k]
+            maxp = po
+            #print("*", po, key)
+                #tried[k] = 0
+        """
+        else:
+            for i in range (4):
+                if tried[i] != 0:
+                    key = tried[i]
+        """
+        """debug
+        print (tried)
+        print(key, maxp)
+        """
+        #print("!", self.matrix, matrix3)
+        #if matrix == matrix3:
+        #    tried[k] = 0
+        #key = heuristic_random()
+        
+        key2, po_empty = heuristic_empty_tile(matrix)
+      
+        if (po_empty*maxp /5)>maxp:
+            key = key2
+            #Samppa 
+    print(po_empty*maxp /5, key, maxp)
+    return key
+   
 def AI_play2(matror, matr, k1, key1):
     tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT] 
     #key=tmp[random.randint(0,3)]
@@ -82,15 +64,15 @@ def AI_play2(matror, matr, k1, key1):
     pointsM = 1
     k = tmp[k1]
     #multiplyer 20 if up left corner has something
-    pointsM = pointsM + 20 * up_left_max(matr)
+    pointsM = pointsM + 200 * up_left_max(matr)
     #multiplyer 10 if down right corner has something  
-    pointsM = pointsM + 10 * down_right_max(matr)  
+    pointsM = pointsM + 100 * down_right_max(matr)  
     #multiplyer 5 if down left corner has something
-    pointsM = pointsM + 5 * down_left_max(matr)
+    pointsM = pointsM + 50 * down_left_max(matr)
     #multiplyer 3 if up right corner has something  
-    pointsM = pointsM + 3 * up_right_max(matr) 
+    pointsM = pointsM + 30 * up_right_max(matr) 
     #sum of differences
-    pointsM = pointsM + 3 * summa (matr)
+    pointsM = pointsM + 30 * summa (matr)
     #if full points are 0
     pointsM = pointsM + 1000 * full(matr)
     #print key in loop and its value 
@@ -125,3 +107,38 @@ def full(mf):
     else:
         return 0
    
+
+
+    
+"""
+def heuristic_random():
+    tmp = [c.KEY_UP, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_LEFT] 
+    key=tmp[random.randint(0,3)]
+    return key
+"""
+def heuristic_empty_tile(matrix):
+    best_score = -1
+    return_key = None
+    for key in commands.keys():
+        game, done, points = commands[key](matrix)  
+
+        if not done:
+           pass
+
+        if done:
+            n_empty=0
+            for i in range(c.GRID_LEN):
+                for j in range(c.GRID_LEN):
+                    if game[i][j]==0:
+                        n_empty+=1
+            if n_empty > best_score:
+                best_score = n_empty
+                return_key = key
+           
+
+    #print("Best move seems to be: ")
+    
+    #print(return_key)    
+
+    return return_key, n_empty
+
