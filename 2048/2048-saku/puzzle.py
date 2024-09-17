abcbc776842bc070cb2d5094bf6210ebf4253cba
sakuexe <saku.karttunen@gmail.com>
2024-09-17 11:08:42 +0300

    added the base for checking out expectimax
    also requirements.txt for venv

--- 2048/2048-saku/puzzle.py
+++ 2048/2048-saku/puzzle.py
@@ -1,11 +1,19 @@
+import pandas as pd
 from tkinter import Frame, Label, CENTER
 import random
 import logic
 import constants as c
+import helpers as h
 import sys
+
+# import AI_expectimax as AI
 import AI_heuristics as AI
-# import AI_minimax as AI
+# import AI_expectimax_SOLUTION as AI
 # import AI_Play_both as AI
+
+sys.setrecursionlimit(10**6)
+
+"""Function that generates a random position withing the grid"""
 
 
 def gen():
@@ -13,176 +21,152 @@ def gen():
 
 
 class GameGrid(Frame):
-    def __init__(self):
+    """Method to close the game window"""
+
+    def close_game(self):
+        self.destroy()
+        self.master.destroy()
+
+    def __init__(self, draw=True, max_depth=2):
+        self.max_depth = max_depth
+        self.draw = draw
         Frame.__init__(self)
-        self.game_over = False
-        self.start = True
-        self.points = 0
+        self.game_over = False      # Is the game over
+        self.start = True           # Has the game started
+        self.points = 0             # Keep track of the points
 
-        self.grid()
-        self.master.title('2048')
-        # self.master.bind("<Key>", self.key_down)
+        self.grid()                 # Initialize the grid
+        self.master.title('2048')   # Set the title for the game window
 
-        self.done = False
+        self.done = False           # Flag to check if move is done
 
+        """Map the key presses to the movement function from the constants file"""
         self.commands = {
             c.KEY_UP: logic.up,
             c.KEY_DOWN: logic.down,
             c.KEY_LEFT: logic.left,
-            c.KEY_RIGHT: logic.right,
-            c.KEY_UP_ALT1: logic.up,
-            c.KEY_DOWN_ALT1: logic.down,
-            c.KEY_LEFT_ALT1: logic.left,
-            c.KEY_RIGHT_ALT1: logic.right,
-            c.KEY_UP_ALT2: logic.up,
-            c.KEY_DOWN_ALT2: logic.down,
-            c.KEY_LEFT_ALT2: logic.left,
-            c.KEY_RIGHT_ALT2: logic.right,
+            c.KEY_RIGHT: logic.right
         }
 
-        self.grid_cells = []
-        self.init_grid()
+        self.grid_cells = []                        # List to store the grid cells
+        self.init_grid()                            # Initialize the grid cells
+        # Create a new game "matrix"
         self.matrix = logic.new_game(c.GRID_LEN)
-        self.history_matrixs = []
+        self.history_matrixs = []                   # List to store the previous matrixes
+        # Update the grid cells with the current matrix
         self.update_grid_cells()
 
-        # self.update_view()
-        self.game_loop()
-        self.mainloop()
+        self.update_view()  # Update the view
+
+        """Create a background Frame for the game with specified color, width and height"""
 
     def init_grid(self):
         background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,
                            width=c.SIZE, height=c.SIZE)
-        background.grid()
+        background.grid()   # Place the created Frame to the grid
 
-        for i in range(c.GRID_LEN):
-            grid_row = []
+        for i in range(c.GRID_LEN):  # Loop trough each row of the grid
+            grid_row = []           # Create an empty list to store the current  game state
+
+            """We loop trough the each column of the grid and then create a frame for each cell, with specified settings"""
             for j in range(c.GRID_LEN):
-                cell = Frame(
-                    background,
-                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                    width=c.SIZE / c.GRID_LEN,
-                    height=c.SIZE / c.GRID_LEN
-                )
-                cell.grid(
-                    row=i,
-                    column=j,
-                    padx=c.GRID_PADDING,
-                    pady=c.GRID_PADDING
-                )
-                t = Label(
-                    master=cell,
-                    text="",
-                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                    justify=CENTER,
-                    font=c.FONT,
-                    width=5,
-                    height=2)
-                t.grid()
+                cell = Frame(background, bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                             width=c.SIZE / c.GRID_LEN, height=c.SIZE / c.GRID_LEN)
+                # Add some padding to the cell frame
+                cell.grid(row=i, column=j, padx=c.GRID_PADDING,
+                          pady=c.GRID_PADDING)
+                # Create a label to display the tile number
+                t = Label(master=cell, text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                          justify=CENTER, font=c.FONT, width=5, height=2)
+                t.grid()                        # Place the label in the cell frame
+                # Add the label to the current row list
                 grid_row.append(t)
+            # Add the current row list to the grid cells list
             self.grid_cells.append(grid_row)
 
     def update_grid_cells(self):
-        for i in range(c.GRID_LEN):
-            for j in range(c.GRID_LEN):
+        for i in range(c.GRID_LEN):             # Loop the rows
+            for j in range(c.GRID_LEN):         # Loop the columns
+                # Get the value of the cell from the matrix
                 new_number = self.matrix[i][j]
-                if new_number == 0:
+                if new_number == 0:             # If the value is 0, set the label text and background color to empty
                     self.grid_cells[i][j].configure(
                         text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+
+                # If the cell does have a value, set the label, text and colors accordingly
                 else:
-                    self.grid_cells[i][j].configure(
-                        text=str(new_number),
-                        bg=c.BACKGROUND_COLOR_DICT[new_number],
-                        fg=c.CELL_COLOR_DICT[new_number]
-                    )
-        self.update_idletasks()
-
-    def key_down(self, event):
-        key = event.keysym
-        # print(event)
-        if key == c.KEY_QUIT:
-            exit()
-        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
-            self.matrix = self.history_matrixs.pop()
-            self.update_grid_cells()
-            print('back on step total step:', len(self.history_matrixs))
-        elif key in self.commands:
-            self.matrix, done = self.commands[key](self.matrix)
-            if done:
-                self.matrix = logic.add_two(self.matrix)
-                # record last move
-                self.history_matrixs.append(self.matrix)
-                self.update_grid_cells()
-                if logic.game_state(self.matrix) == 'win':
-                    self.grid_cells[1][1].configure(
-                        text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(
-                        text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                if logic.game_state(self.matrix) == 'lose':
-                    self.grid_cells[1][1].configure(
-                        text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(
-                        text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-
-    def generate_next(self):
-        index = (gen(), gen())
-        while self.matrix[index[0]][index[1]] != 0:
-            index = (gen(), gen())
-        self.matrix[index[0]][index[1]] = 2
+                    self.grid_cells[i][j].configure(text=str(
+                        new_number), bg=c.BACKGROUND_COLOR_DICT[new_number], fg=c.CELL_COLOR_DICT[new_number])
 
     def update_view(self):
-        if not self.game_over and self.start:
-            self.start = False
-            self.update_grid_cells()
-            self.update()
-
-        elif not self.game_over:
-            key = AI.AI_play(self.matrix)
-            self.commands[key](self.matrix)
-
-            self.matrix, done, points = self.commands[key](self.matrix)
-            self.points += points
-            if done:
-                self.done = True
-                self.matrix = logic.add_two(self.matrix)
-                # record last move
-                self.history_matrixs.append(self.matrix)
-                self.update_grid_cells()
+        if not self.game_over and self.start:       # If the game just started
+            self.start = False                      # Set the start to False
+            self.update_grid_cells()                # Update the grid cells
+            if self.draw:
                 self.update()
+
+        elif not self.game_over:            # If the game isn't over
+            # Get the next move from the AI
+            key = AI.AI_play(self.matrix, self.max_depth)
+
+            self.matrix, done, points = self.commands[key](
+                self.matrix)  # Execute the move and get the result
+            self.points += points   # Add points from the move
+            if done:                # If a move was done
+                self.done = True    # Set to true
+                # Add a new tile to the matrix
+                self.matrix = logic.add_two(self.matrix)
+                self.history_matrixs.append(
+                    self.matrix)    # Record the last move
+                self.update_grid_cells()                    # Update the grid cells
                 if logic.game_state(self.matrix) == 'win':
-                    self.grid_cells[1][1].configure(
-                        text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(
-                        text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[2][1].configure(
-                        text="Points:", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[2][2].configure(
-                        text=self.points, bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    h.print_results_board(
+                        self.grid_cells, self.points, win=True)
                     self.game_over = True
+                    if self.draw:
+                        self.after(1000, self.update())
+                        self.destroy()
 
                 if logic.game_state(self.matrix) == 'lose':
-                    self.grid_cells[1][1].configure(
-                        text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(
-                        text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[2][1].configure(
-                        text="Points:", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[2][2].configure(
-                        text=self.points, bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    h.print_results_board(
+                        self.grid_cells, self.points, win=False)
                     self.game_over = True
 
-    def game_loop(self):
-        while not self.game_over:
-            # print("Your point so far : " + str(self.points))
-            self.after(1, self.update_view)
-            self.update_grid_cells()
-            self.update()
-            # self.update_idletasks()
+                    if self.draw:
+                        self.after(1000, self.update())
+                        self.destroy()
 
-        # print(f"You lose! Points: {self.points}")  # <- benchmark points
-        print(f"Biggest number: {max(max(self.matrix))}")  # <- benchmark the biggest number
-        self.destroy()
-        exit()
+                if self.draw:
+                    self.update()
+
+        if not self.game_over:  # If the game isn't over
+            self.update_view()
 
 
-game_grid = GameGrid()
+#  =========================  MAIN FUNCTION  =============================
+
+
+def main():
+
+    draw = True
+    # draw = False
+    sims = 3
+    max_depth = -1
+
+    sim_results = pd.DataFrame(columns=['Game Parameters', 'Score'])
+
+    for heuristic in ["empty_tile_heuristics"]:
+
+        for i in range(0, sims):
+            game_grid = GameGrid(draw, max_depth=max_depth)
+            tmp = {"Game Parameters": heuristic,
+                   "Score": game_grid.points}
+            df_tmp = pd.DataFrame(tmp, index=[i])
+            sim_results = pd.concat([sim_results, df_tmp])
+
+    print(sim_results)
+    print(sim_results["Score"].mean())
+
+
+if __name__ == "__main__":
+    main()
