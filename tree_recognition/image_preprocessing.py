55980f421714752cd86955de19245e7ce839a4e5
sakuexe <saku.karttunen@gmail.com>
2024-10-02 12:55:52 +0300

    updated the preprocessing script to work multithreaded
    - also it is now easier to combine all sorts of filters and pillow
      changes to it.

--- tree_recognition/image_preprocessing.py
+++ tree_recognition/image_preprocessing.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
 import pathlib
+import concurrent.futures
 
 TRAINING_ROOT = "trees_training"
 IMAGE_SIZE = (1080, 1080)
 
 
-def crop_images(image_path: str) -> None:
-    image = Image.open(image_path)
+def crop_images(image: Image.Image):
     width, height = image.size
 
     left, right, top, bottom = [0] * 4
@@ -28,24 +28,30 @@ def crop_images(image_path: str) -> None:
 
     image.thumbnail(IMAGE_SIZE)
 
-    save_path = f"edited_{image_path}"
+
+def preprocess(filename: str, dirpath: str) -> None:
+    """Applies all the preprocessing to a given file."""
+    filepath = os.path.join(dirpath, filename)
+    image = Image.open(filepath)
+    # TODO: add more preprocessing, like random rotation or color adjustments.
+    # always pass the image-variable, so that all the effects get
+    # applied to the same instance, that will be saved afterwards.
+    crop_images(image)
+
+    # close and save the image when all processing is done
+    save_path = f"edited_{filepath}"
     if not os.path.exists(pathlib.Path(save_path).parent):
         os.makedirs(pathlib.Path(save_path).parent)
 
     image.save(save_path)
-
     image.close()
-
-
-def preprocess(filepath: str) -> None:
-    """Applies all the preprocessing to a given file."""
-    crop_images(filepath)
-    # TODO: add more preprocessing, like random rotation or color adjustments
 
 
 for dirpath, dirnames, filenames in os.walk(TRAINING_ROOT):
     if not filenames:
         continue
 
-    for filename in filenames:
-        preprocess(os.path.join(dirpath, filename))
+    # execute the preprocessing in parallel using multithreading
+    # for speediness
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(preprocess, filenames, [dirpath] * len(filenames))
