597bd3756d1a0727999f4f4a203536ec38ab5c1e
Samppa72 <samppa.sirno@student.hamk.fi>
2024-10-02 19:34:40 +0300

    new version of script
    Put the different filters in separate functions

--- tree_recognition/image_preprocessing.py
+++ tree_recognition/image_preprocessing.py
@@ -8,6 +8,7 @@ TRAINING_ROOT = "trees_training"
 IMAGE_SIZE = (1080, 1080)
 
 
+
 def crop_images(image_path: str) -> None:
     image = Image.open(image_path)
     width, height = image.size
@@ -29,12 +30,17 @@ def crop_images(image_path: str) -> None:
     image = image.crop((left, top, right, bottom))
 
     image.thumbnail(IMAGE_SIZE)
+    return image
+
+def rotate_images(image) -> None:
     #random rotation
     a = [0,90,180,270]
     random.shuffle(a)
     ang = a[0]
     image = image.rotate(ang)
-
+    print(ang)
+    return image
+def filter_images(image) -> None:
     #filter random
     r = random.randint(0,3)
     match r:
@@ -46,30 +52,37 @@ def crop_images(image_path: str) -> None:
             image = image.filter(ImageFilter.SMOOTH)
         case 3:
             pass
+    print(r)
+    return image
+def contrast_images(image) -> None:
     #random color
     #contrast
     c = 1.0
     if random.random()*1>0.5:
         c = random.random()*1 + 0.5
         image = ImageEnhance.Contrast(image).enhance(c)
-    
+    print(c)
+    return image
    
-            
+def save_images(image, image_path: str) -> None:
     save_path = f"edited_{image_path}"
     if not os.path.exists(pathlib.Path(save_path).parent):
         os.makedirs(pathlib.Path(save_path).parent)
-
+    print(save_path)
     image.save(save_path)
-    print(save_path, ang, r, c)
     image.close()
 
 
 def preprocess(filepath: str) -> None:
     """Applies all the preprocessing to a given file."""
-    crop_images(filepath)
-    # TODO: add more preprocessing, like random rotation or color adjustments
-
-
+    image = crop_images(filepath)
+    # TODO: add more preprocessing, like random rotation or color 
+    print("Cropped")
+    image = rotate_images(image)
+    image = filter_images(image)
+    image = contrast_images(image)
+    save_images(image, filepath)
+    
 for dirpath, dirnames, filenames in os.walk(TRAINING_ROOT):
     if not filenames:
         continue
