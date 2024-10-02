214031e5dbfe5c9e470d0a729275738a3cf656da
Samppa72 <samppa.sirno@student.hamk.fi>
2024-10-02 18:47:30 +0300

    Edited trees
    Random filter, rotate, contrast, results in edited_trees_training, code in image preprocessing.py

--- tree_recognition/image_preprocessing.py
+++ tree_recognition/image_preprocessing.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import os
 import pathlib
-import concurrent.futures
+import random
+from PIL import ImageFilter, ImageEnhance
 
 TRAINING_ROOT = "trees_training"
 IMAGE_SIZE = (1080, 1080)
 
 
-def crop_images(image: Image.Image):
+def crop_images(image_path: str) -> None:
+    image = Image.open(image_path)
     width, height = image.size
 
     left, right, top, bottom = [0] * 4
@@ -27,31 +29,50 @@ def crop_images(image: Image.Image):
     image = image.crop((left, top, right, bottom))
 
     image.thumbnail(IMAGE_SIZE)
+    #random rotation
+    a = [0,90,180,270]
+    random.shuffle(a)
+    ang = a[0]
+    image = image.rotate(ang)
 
-
-def preprocess(filename: str, dirpath: str) -> None:
-    """Applies all the preprocessing to a given file."""
-    filepath = os.path.join(dirpath, filename)
-    image = Image.open(filepath)
-    # TODO: add more preprocessing, like random rotation or color adjustments.
-    # always pass the image-variable, so that all the effects get
-    # applied to the same instance, that will be saved afterwards.
-    crop_images(image)
-
-    # close and save the image when all processing is done
-    save_path = f"edited_{filepath}"
+    #filter random
+    r = random.randint(0,3)
+    match r:
+        case 0:
+            image = image.filter(ImageFilter.BLUR)
+        case 1:
+            image = image.filter(ImageFilter.SHARPEN)
+        case 2:
+            image = image.filter(ImageFilter.SMOOTH)
+        case 3:
+            pass
+    #random color
+    #contrast
+    c = 1.0
+    if random.random()*1>0.5:
+        c = random.random()*1 + 0.5
+        image = ImageEnhance.Contrast(image).enhance(c)
+    
+   
+            
+    save_path = f"edited_{image_path}"
     if not os.path.exists(pathlib.Path(save_path).parent):
         os.makedirs(pathlib.Path(save_path).parent)
 
     image.save(save_path)
+    print(save_path, ang, r, c)
     image.close()
+
+
+def preprocess(filepath: str) -> None:
+    """Applies all the preprocessing to a given file."""
+    crop_images(filepath)
+    # TODO: add more preprocessing, like random rotation or color adjustments
 
 
 for dirpath, dirnames, filenames in os.walk(TRAINING_ROOT):
     if not filenames:
         continue
 
-    # execute the preprocessing in parallel using multithreading
-    # for speediness
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(preprocess, filenames, [dirpath] * len(filenames))
+    for filename in filenames:
+        preprocess(os.path.join(dirpath, filename))
