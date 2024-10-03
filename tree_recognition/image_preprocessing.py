ebc8b0122f483748cf75401126ebe999e1823d8e
sakuexe <saku.karttunen@gmail.com>
2024-10-03 09:26:56 +0300

    fixed errors in preprocessing (made by my tweaks, whoops)
    - fixed error of filters not getting applied
    - added saturation to the filters
    - fixed some typos
    - reran the script

--- tree_recognition/image_preprocessing.py
+++ tree_recognition/image_preprocessing.py
@@ -10,7 +10,7 @@ IMAGE_SIZE = (1080, 1080)
 SAVE_DIR = "trees_training/edited"
 
 
-def crop_images(image: Image.Image) -> None:
+def crop_image(image: Image.Image) -> Image.Image:
     """Crops and resizes the passed image.
     The cropping will be done to a square aspect ratio."""
     width, height = image.size
@@ -32,41 +32,58 @@ def crop_images(image: Image.Image) -> None:
     image = image.crop((left, top, right, bottom))
     # resize the image
     image.thumbnail(IMAGE_SIZE)
+    return image
 
 
-def rotate_images(image: Image.Image) -> None:
+def rotate_image(image: Image.Image) -> Image.Image:
     """Applies a random rotation to the image.
     The possible rotations are in 90 degree intervals."""
     rotate_by_angle = [0, 90, 180, 270]
     image = image.rotate(random.choice(rotate_by_angle))
+    return image
 
 
-def filter_images(image: Image.Image) -> None:
+def filter_image(image: Image.Image) -> Image.Image:
     """Applies a random image filter to the passed image.
     This filter is either blur, sharpen or smooth."""
     filters = (ImageFilter.BLUR, ImageFilter.SHARPEN, ImageFilter.SMOOTH)
     image = image.filter(random.choice(filters))
+    return image
 
 
-def contrast_images(image: Image.Image) -> None:
+def contrast_image(image: Image.Image, multiplier=1.5, min_contrast=0.5) -> Image.Image:
     """Applies a random amount of image contrast to the passed image.
-    This value can be between 0.5 and 1.5."""
-    contrast = random.random() + 0.5
-    image = ImageEnhance.Contrast(image).enhance(contrast)
+    With the multiplier you can change the max strength of the saturation.
+    The min_contrast parameter sets the minimum possible contrast factor
+    The base value can be between 0.5 and 1.5."""
+    factor = random.random() * multiplier + min_contrast
+    image = ImageEnhance.Contrast(image).enhance(factor)
+    return image
+
+
+def saturate_image(image: Image.Image, multiplier=1.75, min_saturation=0.25) -> Image.Image:
+    """Applies random amount of saturation to the passed image.
+    With the multiplier you can change the max strength of the saturation.
+    The min_saturation parameter sets the base minimum saturation factor
+    The base value is between 0 and 1"""
+    factor = random.random() * multiplier + min_saturation
+    image = ImageEnhance.Color(image).enhance(factor)
+    return image
 
 
 def preprocess(filename: str, dirpath) -> None:
     """Applies all the preprocessing to a given image file
     inside the given directory path."""
+    # open the image
     filepath: str = os.path.join(dirpath, filename)
     image: Image.Image = Image.open(filepath)
 
-    # apply filters -
-    # the image variable can just be passed as a reference here
-    crop_images(image)
-    rotate_images(image)
-    filter_images(image)
-    contrast_images(image)
+    # apply filters
+    image = crop_image(image)
+    image = rotate_image(image)
+    image = filter_image(image)
+    image = contrast_image(image)
+    image = saturate_image(image)
 
     # save the images at the end
     class_name: str = pathlib.PurePath(dirpath).name
