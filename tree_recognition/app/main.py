03b123d0caa7e261820960a764982bfa4935b678
Samppa72 <samppa.sirno@student.hamk.fi>
2024-10-04 13:28:28 +0300

    Muutoksia
    Pohja fasapille
    toimii fastapi dev main.py komennolla

--- tree_recognition/app/main.py
+++ tree_recognition/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from typing import Union
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
