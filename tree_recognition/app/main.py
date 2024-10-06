d4291b713de3b3173b9321c2778f9f5058a3a875
Samppa72 <samppa.sirno@student.hamk.fi>
2024-10-06 04:53:27 +0300

    Kokeilua
    Lataa kuvat ruudulle ja tallettaa ne uuteen paikkan. käynnistys:
    fastapi dev main.py ja osoite /docs
    Tämä on tarkoius muuttaa nettisivuun sopivaksi ja talletuksen tilalle funktiokutsut

--- tree_recognition/app/main.py
+++ tree_recognition/app/main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
-from typing import Union
+import os
+from PIL import Image
+
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+    
+   
+@app.post("/several_items/")
+async def several_images(path: str="\\tree_recognition\\koe\\", whichTree: str = "maple"):
+    os.chdir("..")
+    directory = os.path.dirname(os.getcwd())+ "\\tree_recognition\\trees_training\\edited\\"+whichTree+"\\"
+    lst = os.listdir(directory)
+    number_files = len(lst)
+    for id in range(number_files):
+        if id <=9:
+            N = "_00"
+        else:
+            N = "_0"
+        names = os.path.dirname(os.getcwd()) + path + whichTree + str(id)+".jpg"
+        fileName = whichTree+N+str(id)+".jpg"
+        fileName2 = whichTree+N+str(id)+".jpeg"
+        file = os.path.dirname(os.getcwd()) + "\\tree_recognition\\trees_training\\edited\\"+whichTree+"\\"+ fileName
+        file2 = os.path.dirname(os.getcwd()) + "\\tree_recognition\\trees_training\\edited\\"+whichTree+"\\" + fileName2
+        if os.path.isfile(file):
+            image = Image.open(file)
+        elif os.path.isfile(file2):
+            image = Image.open(file2)
+        else:
+            id = id + 1
+            continue
+        image.show()
+        image.save(names)
+        image.close()
+    return file
